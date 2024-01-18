1467acce1889d34bf8a97f3c389027460ca72abd
Michael <kontakt@ozum.pl>
2024-01-18 23:04:57 +0100

    added: scss, change: css to scss, new folder structure 'styles'

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles/index.scss'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {Home} from "./pages/Home.jsx";
 import {About} from "./pages/About.jsx";
