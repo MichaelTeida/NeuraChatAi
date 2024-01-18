68189f2fe057957645dc13a1d1385e0775dbf313
Michael <kontakt@ozum.pl>
2024-01-18 21:20:02 +0100

    update: React router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Home} from "./pages/Home.jsx";
+import {About} from "./pages/About.jsx";
+
+
+const routes = [
+    {
+        path: "/NeuraChatAi/",
+        element: <App/>,
+        children: [
+            {
+                path: "/NeuraChatAi/",
+                element: <Home/>,
+            },
+            {
+                path: "/NeuraChatAi/about",
+                element: <About/>,
+            }
+        ]
+    }
+]
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export function Home() {
+    return (
+        <>
+            <div>NeuraChatAi</div>
+        </>
+    )
+}
