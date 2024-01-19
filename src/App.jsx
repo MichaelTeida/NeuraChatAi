4bda8618fe15580ac162a538d312b50e1c0f8feb
Michael <kontakt@ozum.pl>
2024-01-19 01:17:23 +0100

    fix: normalize, added: global reset from Joy UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import {Link as RouterLink, Outlet} from "react-router-dom";
 
 function App() {
 
-  return (
-        <div>Hello there ;)</div>
-        <nav>
-            <RouterLink to="/NeuraChatAi/">Homepage</RouterLink>
-            <div></div>
-            <RouterLink to="/NeuraChatAi/about">About</RouterLink>
-        </nav>
-        {<Outlet />}
-  )
+    return (
+        <>
+            <div>Hello there ;)</div>
+            <nav>
+                <RouterLink to="/NeuraChatAi/">Homepage</RouterLink>
+                <div></div>
+                <RouterLink to="/NeuraChatAi/about">About</RouterLink>
+            </nav>
+            <Outlet/>
+
+        </>
+    )
 }
 
 export default App
