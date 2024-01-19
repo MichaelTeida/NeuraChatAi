0d84624558248a27f2934526c2100e5c6f96c77e
Michael <kontakt@ozum.pl>
2024-01-20 00:57:29 +0100

    fix: input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import {Button, Card, CardContent, Input as JoyInput} from "@mui/joy";
+import {Button, Card, CardActions, CardContent, Input as JoyInput} from "@mui/joy";
 import EditIcon from '@mui/icons-material/Edit';
 import SendIcon from '@mui/icons-material/Send';
 import TouchAppIcon from '@mui/icons-material/TouchApp';
@@ -6,16 +6,13 @@ import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 function Input() {
     return (
-        <Card>
-            <CardContent>
+        <Card >
+            <CardContent sx={{display: "flex", flexDirection: {xs: "column", sm: "row"}}}>
                 <JoyInput
                     startDecorator={<EditIcon/>}
-                    endDecorator={<Button><ArrowUpwardIcon sx={{mx: "3px"}}/>Send</Button>}
                     placeholder="Type something here…"
-                    sx={{
-                        "--Input-minHeight": "48px",
-                        "--Input-decoratorChildHeight": "48px",
-                    }}/>
+                    sx={{"--Input-minHeight": "48px", flexGrow: 1}}/>
+                <Button><ArrowUpwardIcon sx={{mx: "3px"}}/>Send</Button>
             </CardContent>
         </Card>
     )
