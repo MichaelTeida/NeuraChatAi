b05c29abea5f31e85d675ac0f479c88f48597079
Michael <kontakt@ozum.pl>
2024-01-20 20:54:43 +0100

    update: about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,11 @@
-import {Box} from "@mui/joy";
+import {Card, Box} from "@mui/joy";
 
 export function About() {
     return (
-        <>
+        <Card spacing={2} sx={{my: 2}}>
             <Box>About NeuraChatAi</Box>
             <Box>About NeuraChatAi</Box>
             <Box>About NeuraChatAi</Box>
-        </>
+        </Card>
     );
 }
