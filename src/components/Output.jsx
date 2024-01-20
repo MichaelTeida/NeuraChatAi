5e5cb438df9231c3b1c0b09e1abc1200b5844511
Michael <kontakt@ozum.pl>
2024-01-20 19:29:22 +0100

    added: output skeleton

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,9 +1,38 @@
-import {Card} from "@mui/joy";
+import {Box, Card, Sheet, Stack, Typography} from "@mui/joy";
 
-function Output () {
+function Output() {
     return (
         <Card>
-        Test
+            <Box sx={{maxWidth: {xs: 'auto', sm: "60%"}, minWidth: 'auto'}}>
+                <Stack direction="row" justifyContent="space-beteween" spacing={2}>
+                    <Typography level="body-xs">
+                        NeuraChat
+                    </Typography>
+                    <Typography level="body-xs">
+                        Timestamp
+                    </Typography>
+                </Stack>
+                <Sheet variant="outlined" sx={{p: 1, borderRadius: "sm", backgroundColor: "background.level1"}}>
+                    <Typography component="p" fontSize={{xs: "sm", sm: "md"}} sx={{ wordBreak: "break-word" }}>
+                        Messageeeeeeee
+                    </Typography>
+                </Sheet>
+            </Box>
+            <Box sx={{maxWidth: {xs: 'auto', sm: "60%"}, minWidth: 'auto'}}>
+                <Stack direction="row" justifyContent="space-beteween" spacing={2}>
+                    <Typography level="body-xs">
+                        User
+                    </Typography>
+                    <Typography level="body-xs">
+                        Timestamp
+                    </Typography>
+                </Stack>
+                <Sheet variant="outlined" sx={{p: 1, borderRadius: "sm", backgroundColor: "background.level1"}}>
+                    <Typography component="p" fontSize={{xs: "sm", sm: "md"}} sx={{ wordBreak: "break-word" }}>
+                        MessageeeeeeeeMessageeeeeeeeMessageeeeeeeeMessageeeeeeee
+                    </Typography>
+                </Sheet>
+            </Box>
         </Card>
     )
 }
