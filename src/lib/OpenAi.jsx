26c5733689f9cd47e905ab3e63e2b7651bc01eaa
Michael <kontakt@ozum.pl>
2024-01-21 02:39:23 +0100

    fix: OpenAi configuration

--- src/lib/OpenAi.jsx
+++ src/lib/OpenAi.jsx
@@ -7,16 +7,16 @@ const openai = new OpenAI({
 
 export async function sendMessage(message) {
     const params = {
-        model: 'gpt-3.5-turbo-instruct',
+        model: 'gpt-3.5-turbo',
         temperature: 0.5, // how creative OpenAi should be (0 - 1)
         max_tokens: 400,
-        frequency_penalty: 0, // parametr frequency_penalty pozwala sterować różnorodnością generowanego tekstu przez model GPT, modyfikując rozkład prawdopodobieństwa, aby rzadziej generować słowa, które model częściej widział podczas treningu.
+        frequency_penalty: 0.01, // parametr frequency_penalty pozwala sterować różnorodnością generowanego tekstu przez model GPT, modyfikując rozkład prawdopodobieństwa, aby rzadziej generować słowa, które model częściej widział podczas treningu.
         top_p: 1, // shrinks or grows the “pool” of available tokens to choose from, the domain to select over. 1=big pool, 0=small pool
     };
 
-    const completion = await openai.completions.create({
-        prompt: message,
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{"role": "user", "content": message}],
         ...params
     })
-    return completion.choices[0].text
+    return chatCompletion.choices[0].message.content
 }
