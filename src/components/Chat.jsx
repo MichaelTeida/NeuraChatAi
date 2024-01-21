868b2327c060a2a20d78ac228b9c9272bb3e38e2
Michael <kontakt@ozum.pl>
2024-01-21 18:47:24 +0100

    added: working messages from AI and user

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,66 +1,73 @@
 import {Box, Button, Card, CardContent, Input as JoyInput, Sheet, Stack, Typography} from "@mui/joy";
-import EditIcon from "@mui/icons-material/Edit.js";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward.js";
-import {useState} from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import {useEffect, useState} from "react";
 import {sendMessage} from "../lib/OpenAi.jsx";
 
 function Chat() {
-
     const [input, setInput] = useState("")
+    const [output, setOutput] = useState("")
+    const [messages, setMessages] = useState([{
+        isHuman: false,
+        content: "Example text from NeuraChat"
+    }])
+
+    useEffect(() => {
+        if (output) {
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                {isHuman: false, content: output}
+            ]);
+        }
+    }, [output])
 
     const handleSend = async () => {
-        const respone = await sendMessage(input)
-        console.log(respone)
+        setMessages((prevMessages) => [
+            ...prevMessages,
+            {isHuman: true, content: input}
+        ]);
+        setOutput(await sendMessage(input))
+        setInput("")
     }
 
     return (
-        <Stack spacing={2} sx={{maxHeight: 'calc(100dvh - var(--Header-height))', py: 2}} >
+        <Stack spacing={2} sx={{height: 'calc(100dvh - var(--Header-height))', py: 2}}>
             <Card sx={{
-                display: 'flex',
                 flex: 1,
                 minHeight: 0,
                 overflowY: 'scroll',
-                flexDirection: 'column-reverse',
+                flexDirection: 'column',
             }}>
-                <Box sx={{maxWidth: {xs: 'auto', sm: "60%"}, minWidth: 'auto'}}>
-                    <Stack direction="row" justifyContent="space-beteween" spacing={2}>
-                        <Typography level="body-xs">
-                            NeuraChat
-                        </Typography>
-                        <Typography level="body-xs">
-                            Timestamp
-                        </Typography>
-                    </Stack>
-                    <Sheet variant="outlined" sx={{p: 1, borderRadius: "sm", backgroundColor: "background.level1"}}>
-                        <Typography component="p" fontSize={{xs: "sm", sm: "md"}} sx={{wordBreak: "break-word"}}>
-                            Messageeeeeeee
-                        </Typography>
-                    </Sheet>
-                </Box>
-                <Box sx={{maxWidth: {xs: 'auto', sm: "60%"}, minWidth: 'auto'}}>
-                    <Stack direction="row" justifyContent="space-beteween" spacing={2}>
-                        <Typography level="body-xs">
-                            User
-                        </Typography>
-                        <Typography level="body-xs">
-                            Timestamp
-                        </Typography>
-                    </Stack>
-                    <Sheet variant="outlined" sx={{p: 1, borderRadius: "sm", backgroundColor: "background.level1"}}>
-                        <Typography component="p" fontSize={{xs: "sm", sm: "md"}} sx={{wordBreak: "break-word"}}>
-                            Messageeeeeeee
-                        </Typography>
-                    </Sheet>
-                </Box>
+                {messages.map((el, index) => {
+                    return (
+                        <Box key={index} sx={{maxWidth: {xs: 'auto', sm: "60%"}, minWidth: 'auto'}}>
+                            <Stack direction="row" justifyContent="space-beteween" spacing={2}>
+                                <Typography level="body-xs">
+                                    {el.isHuman ? "You" : "NeuraChat"}
+                                </Typography>
+                                <Typography level="body-xs">
+                                    Timestamp
+                                </Typography>
+                            </Stack>
+                            <Sheet variant="outlined" sx={{p: 1, borderRadius: "sm", backgroundColor: "background.level1"}}>
+                                <Typography component="p" fontSize={{xs: "sm", sm: "md"}} sx={{wordBreak: "break-word"}}>
+                                    {el.content}
+                                </Typography>
+                            </Sheet>
+                        </Box>
+                    )
+                })}
             </Card>
-            <Card >
+            <Card>
                 <CardContent sx={{display: "flex", flexDirection: {xs: "column", sm: "row"}}}>
                     <JoyInput
                         startDecorator={<EditIcon/>}
                         placeholder="Type something here…"
                         sx={{"--Input-minHeight": "48px", flexGrow: 1}}
                         value={input}
-                        onChange={(e)=>{setInput(e.target.value)}}
+                        onChange={(e) => {
+                            setInput(e.target.value)
+                        }}
                     />
                     <Button onClick={handleSend}><ArrowUpwardIcon sx={{mx: "3px"}}/>Send</Button>
                 </CardContent>
--- src/lib/OpenAi.jsx
+++ src/lib/OpenAi.jsx
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 export async function sendMessage(message) {
     const params = {
         model: 'gpt-3.5-turbo',
-        temperature: 0.5, // how creative OpenAi should be (0 - 1)
+        temperature: 0.7, // how creative OpenAi should be (0 - 1)
         max_tokens: 400,
         frequency_penalty: 0.01, // parametr frequency_penalty pozwala sterować różnorodnością generowanego tekstu przez model GPT, modyfikując rozkład prawdopodobieństwa, aby rzadziej generować słowa, które model częściej widział podczas treningu.
         top_p: 1, // shrinks or grows the “pool” of available tokens to choose from, the domain to select over. 1=big pool, 0=small pool
