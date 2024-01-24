a75651b6db609d90211efb35c735856f1126510b
Michael <kontakt@ozum.pl>
2024-01-24 18:18:08 +0100

    update: working settings

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,35 +1,49 @@
-import {Divider, Select, Sheet, Slider, Typography, Option, Box} from "@mui/joy";
-import {useState} from "react";
+import {Divider, Select, Sheet, Slider, Typography, Option, Box, Chip} from "@mui/joy";
+import {useEffect, useState} from "react";
+import {setOpenAiParams} from "../lib/OpenAi.jsx";
 
 const Sidebar = () => {
-
-    const [temperature, settemperature] = useState(0.7);
-    const [frequencyPenalty, setFrequencyPenalty] = useState(0.01);
-    const [maxTokens, setMaxTokens] = useState(500);
-    const [topG, setTopG] = useState(1);
     const [model, setModel] = useState('gpt-3.5-turbo');
+    const [temperature, setTemperature] = useState(0.7);
+    const [maxTokens, setMaxTokens] = useState(500);
+    const [frequencyPenalty, setFrequencyPenalty] = useState(0.01);
+    const [topG, setTopG] = useState(1);
+
+    useEffect(() => {
+        setOpenAiParams({
+            model: model,
+            temperature: temperature,
+            max_tokens: maxTokens,
+            frequency_penalty: frequencyPenalty,
+            top_p: topG,
+        });
+    }, [model, temperature, maxTokens, frequencyPenalty, topG])
 
     return <Sheet sx={{borderRight: '1px solid', borderColor: 'divider', overflowY: 'scroll', width: "25rem", p: 3}}>
-        {/*<Typography >Ustawienia:</Typography>*/}
-        <Divider variant="soft" color="neutral" sx={{mb: 2}}>Settings</Divider>
+        <Divider sx={{mb: 2}}><Chip variant="outlined">Settings</Chip></Divider>
 
         <Box sx={{my: 2, mb: 3}}>
-            <Typography level="title-sm">Model:</Typography>
-            <Select defaultValue={model} size="sm" onChange={(event, value) => setModel(value)}>
+            <Typography component="div" level="title-sm" sx={{mb: 1}}>Model:</Typography>
+            <Select defaultValue={model} size="sm" onChange={(event, value) => setModel(value)} sx={{}}>
                 <Option value="gpt-3.5-turbo">gpt-3.5-turbo</Option>
                 <Option value="gpt-3.5-turbo-0301">gpt-3.5-turbo-0301</Option>
                 <Option value="gpt-3.5-turbo-0613">gpt-3.5-turbo-0613</Option>
                 <Option value="gpt-3.5-turbo-1106">gpt-3.5-turbo-1106</Option>
                 <Option value="gpt-3.5-turbo-16k">gpt-3.5-turbo-16k</Option>
                 <Option value="gpt-3.5-turbo-16k-0613">gpt-3.5-turbo-16k-0613</Option>
+                <Option value="gpt-4" disabled>gpt-4</Option>
+                <Option value="gpt-4-0613" disabled>gpt-4-0613</Option>
+                <Option value="gpt-4-1106-preview" disabled>gpt-4-1106-preview</Option>
+                <Option value="gpt-4-32k" disabled>gpt-4-32k</Option>
+                <Option value="gpt-4-32k-0613" disabled>gpt-4-32k-0613</Option>
             </Select>
         </Box>
 
         <Box sx={{my: 1}}>
-            <Typography level="title-sm">Temperature: {temperature}</Typography>
+            <Typography component="div" level="title-sm">Temperature: <Chip variant="soft" sx={{px: 2, mb: 0.2}}>{temperature}</Chip></Typography>
             <Slider
                 value={temperature}
-                onChange={(event, value) => settemperature(value)}
+                onChange={(event, value) => setTemperature(value)}
                 size="sm"
                 variant="solid"
                 defaultValue={temperature}
@@ -45,7 +59,7 @@ const Sidebar = () => {
         </Box>
 
         <Box sx={{my: 1}}>
-            <Typography level="title-sm">Frequency Penalty: {frequencyPenalty}</Typography>
+            <Typography component="div" level="title-sm">Frequency Penalty: <Chip variant="soft" sx={{px: 2, mb: 0.2}}>{frequencyPenalty}</Chip></Typography>
             <Slider
                 value={frequencyPenalty}
                 onChange={(event, value) => setFrequencyPenalty(value)}
@@ -64,7 +78,7 @@ const Sidebar = () => {
         </Box>
 
         <Box sx={{my: 1}}>
-            <Typography level="title-sm">Max tokens: {maxTokens}</Typography>
+            <Typography component="div" level="title-sm">Max tokens: <Chip variant="soft" sx={{px: 2, mb: 0.2}}>{maxTokens}</Chip></Typography>
             <Slider
                 value={maxTokens}
                 onChange={(event, value) => setMaxTokens(value)}
@@ -73,7 +87,7 @@ const Sidebar = () => {
                 defaultValue={maxTokens}
                 step={1}
                 min={0}
-                max={5000}
+                max={3000}
                 valueLabelDisplay="auto"
                 sx={{
                     "--Slider-thumbWidth": "10px",
@@ -83,7 +97,7 @@ const Sidebar = () => {
         </Box>
 
         <Box sx={{my: 1}}>
-            <Typography level="title-sm">Top_p: {topG}</Typography>
+            <Typography component="div" level="title-sm">Top_p: <Chip variant="soft" sx={{px: 2, mb: 0.2}}>{topG}</Chip></Typography>
             <Slider
                 value={topG}
                 onChange={(event, value) => setTopG(value)}
--- src/lib/OpenAi.jsx
+++ src/lib/OpenAi.jsx
@@ -5,19 +5,24 @@ const openai = new OpenAI({
     dangerouslyAllowBrowser: true
 })
 
-export async function sendMessage(message) {
-    const params = {
+let openAiParams = {
         model: 'gpt-3.5-turbo',
         temperature: 0.7, // how creative OpenAi should be (0 - 1)
-        max_tokens: 10000,
+        max_tokens: 2000,
         frequency_penalty: 0.01, // parametr frequency_penalty pozwala sterować różnorodnością generowanego tekstu przez model GPT, modyfikując rozkład prawdopodobieństwa, aby rzadziej generować słowa, które model częściej widział podczas treningu.
         top_p: 1, // shrinks or grows the “pool” of available tokens to choose from, the domain to select over. 1=big pool, 0=small pool
-    };
+};
 
+export function setOpenAiParams(newParams) {
+    openAiParams = { ...openAiParams, ...newParams };
+}
+
+export async function sendMessage(message) {
     const chatCompletion = await openai.chat.completions.create({
         messages: [{"role": "system", "content": "You are NeuraChat, an AI chat system developed by Michael Teida."},{"role": "user", "content": message}],
-        ...params
+        ...openAiParams
     })
+    console.log(openAiParams)
 
     return chatCompletion.choices[0].message.content
 }
