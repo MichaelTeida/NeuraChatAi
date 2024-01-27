d6d55a5cb61a010c8c996c1d68cce155ef487ac4
Michael <kontakt@ozum.pl>
2024-01-27 23:03:28 +0100

    update: backgroundAnimation opacity

--- src/components/BackgroundAnimation.jsx
+++ src/components/BackgroundAnimation.jsx
@@ -1,9 +1,17 @@
-import {Box} from "@mui/joy";
+import {Box, useColorScheme} from "@mui/joy";
 import '../styles/backgroundAnimation.scss'
+import {useEffect, useState} from "react";
 
 function BackgroundAnimation() {
+    const {mode} = useColorScheme();
+    const [opacity, setOpacity] = useState("10%")
+
+    useEffect(() => {
+        mode === 'light' ? setOpacity("20%") : setOpacity("40%")
+    },[mode])
+
     return (
-            <Box className="background">
+            <Box sx={{opacity: opacity}} className="background">
                 <Box component="span"></Box>
                 <Box component="span"></Box>
                 <Box component="span"></Box>
